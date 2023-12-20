152a194c04e8a06530f19ebde6466c0adfc4e640
Emranhossainshuvo <shuvoorba28@gmail.com>
2023-12-20 23:11:39 +0600

    basic footer added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../shared/Navbar/Navbar';
+import Footer from '../shared/Footer/Footer';
 
 const Main = () => {
     return (
-        <div>
+        <div className='max-w-screen-2xl mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
