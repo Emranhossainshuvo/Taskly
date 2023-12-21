109eef2e0a2b763ba263fa7aca0a583e224c887c
Emranhossainshuvo <shuvoorba28@gmail.com>
2023-12-21 15:16:50 +0600

    banner added with image

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,22 @@
+import React from "react";
 
 const LandingPage = () => {
   return (
-    <div className="bg-gray-100 h-screen flex items-center justify-center">
+    <div
+      className="bg-cover bg-center my-20 h-screen flex items-center justify-center"
+      style={{
+        backgroundImage:
+          'url("https://i.ibb.co/HhwFyBz/i-can-handle-multi-tasks-cropped-shot-successful-girl-typing-keyboard-making-notes-while-looking-com.jpg")',
+        opacity: 0.8,
+      }}
+    >
       <div className="max-w-2xl bg-white p-8 shadow-md rounded-md">
-        <h1 className="text-4xl font-bold mb-4">Welcome to SCC Technovision Inc.</h1>
+        <h1 className="text-4xl font-bold mb-4 text-gray-800">
+          Welcome to SCC Technovision Inc.
+        </h1>
         <p className="text-gray-600 mb-8">
-          We build collaborative task management platforms to enhance your productivity.
+          We build collaborative task management platforms to enhance your
+          productivity.
         </p>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
